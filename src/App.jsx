18b2502f4dc6e6d9741f17bf6c75b0742fa52f8f
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2023-09-05 15:24:46 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2">
+
+        <div className='bg-black p-52'></div>
+        <div className='bg-slate-600 p-52'></div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
